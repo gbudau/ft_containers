57455c0c79b1386262d48ab06c5e4c2745fb0239
gbudau <54190400+gbudau@users.noreply.github.com>
2021-05-21 20:18:06 +0200

    Add capacity() member function
    Added capacity() member function
    Returning the current capacity of the vector

--- main.cpp
+++ main.cpp
@@ -9,8 +9,9 @@ int	main() {
 	(void)v;
 	ft::vector<int> v2(3, 42);
 
-	std::cout << v1.size() << "\n";
-	std::cout << v2.size() << "\n";
-	std::cout << v1.max_size() << "\n";
-	std::cout << v.max_size() << "\n";
+	std::cout << "v1 Size: " << v1.size() << "\n";
+	std::cout << "v2 Size: " << v2.size() << "\n";
+	std::cout << "v2 Capacity: " << v2.capacity() << "\n";
+	std::cout << "v1 Max Size: " << v1.max_size() << "\n";
+	std::cout << "v Max Size: " << v.max_size() << "\n";
 }
--- vector.hpp
+++ vector.hpp
@@ -49,7 +49,7 @@ public:
 	size_type	size() const;
 	size_type	max_size() const;
 //	void		resize(size_type sz, T c = T());
-//	size_type	capacity() const;
+	size_type	capacity() const;
 //	bool		empty() const;
 //	void		reserve(size_type n);
 //
@@ -169,6 +169,11 @@ typename vector<T, Allocator>::size_type	vector<T, Allocator>::size() const {
 template <class T, class Allocator>
 typename vector<T, Allocator>::size_type	vector<T, Allocator>::max_size() const {
 	return m_allocator.max_size();
+}
+
+template <class T, class Allocator>
+typename vector<T, Allocator>::size_type	vector<T, Allocator>::capacity() const {
+	return m_end_of_storage - begin();
 }
 
 }  // namespace ft
