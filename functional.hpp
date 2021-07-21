316a864534b97893fde2aaa776fb071798ce67f9
gbudau <54190400+gbudau@users.noreply.github.com>
2021-07-21 20:31:16 +0200

    Add unary_function to functional.hpp
    Add unary_function base class to functional.hpp
    unary_function is a base class for creating function objects with one argument.

--- functional.hpp
+++ functional.hpp
@@ -2,6 +2,12 @@
 
 namespace ft {
 
+template <class Arg, class Result>
+struct unary_function {
+	typedef Arg    argument_type;
+	typedef Result result_type;
+};
+
 template <class Arg1, class Arg2, class Result>
 struct binary_function {
 	typedef Arg1   first_argument_type;
