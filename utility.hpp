3e02c96aacd5b5c4483b2bf3b1020e2cead99939
gbudau <54190400+gbudau@users.noreply.github.com>
2021-07-23 23:08:17 +0200

    Fix make_pair
    Fix implicit conversion
    https://cplusplus.github.io/LWG/issue181

--- utility.hpp
+++ utility.hpp
@@ -46,7 +46,7 @@ bool operator>=(const pair<T1, T2> &x, const pair<T1, T2> &y) {
 }
 
 template <class T1, class T2>
-pair<T1, T2> make_pair(const T1 &x, const T2 &y) {
+pair<T1, T2> make_pair(T1 x, T2 y) {
 	return pair<T1, T2>(x, y);
 }
 
