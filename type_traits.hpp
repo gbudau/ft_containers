f775e37ef82915805908e6cb8db020935089bf57
gbudau <54190400+gbudau@users.noreply.github.com>
2021-07-28 16:49:08 +0200

    Add is_integral to type_traits.hpp
    Add is_integral to type_traits.hpp

--- type_traits.hpp
+++ type_traits.hpp
@@ -10,4 +10,78 @@ struct enable_if<true, T> {
 	typedef T type;
 };
 
+template <class T, T val>
+struct integral_constant {
+	typedef integral_constant<T, val> type;
+	typedef T                         value_type;
+	static const T                    value = val;
+									  operator value_type() const {
+        return value;
+	}
+};
+
+template <class T, T val>
+T const integral_constant<T, val>::value;
+
+template <bool val>
+struct integral_constant<bool, val> {
+	typedef integral_constant<bool, val> type;
+	typedef bool                         value_type;
+	static const bool                    value = val;
+										 operator value_type() const {
+        return value;
+	}
+};
+
+template <bool val>
+bool const                             integral_constant<bool, val>::value;
+
+typedef integral_constant<bool, true>  true_type;
+typedef integral_constant<bool, false> false_type;
+
+template <class T>
+struct is_integral : public false_type {};
+
+template <class T>
+struct is_integral<const T> : public is_integral<T> {};
+
+template <class T>
+struct is_integral<volatile const T> : public is_integral<T> {};
+
+template <class T>
+struct is_integral<volatile T> : public is_integral<T> {};
+
+template <>
+struct is_integral<unsigned char> : public true_type {};
+
+template <>
+struct is_integral<unsigned short> : public true_type {};
+
+template <>
+struct is_integral<unsigned int> : public true_type {};
+
+template <>
+struct is_integral<unsigned long> : public true_type {};
+
+template <>
+struct is_integral<signed char> : public true_type {};
+
+template <>
+struct is_integral<short> : public true_type {};
+
+template <>
+struct is_integral<int> : public true_type {};
+
+template <>
+struct is_integral<long> : public true_type {};
+
+template <>
+struct is_integral<char> : public true_type {};
+
+template <>
+struct is_integral<bool> : public true_type {};
+
+template <>
+struct is_integral<wchar_t> : public true_type {};
+
 }  // namespace ft
